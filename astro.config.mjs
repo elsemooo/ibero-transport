54717a7d8b96227c62deb94bdfcf8d0f4fc927c5
Khalid Samanamud <khalidsamanamud@gmail.com>
2025-01-17 12:00:22 -0500

    Update all modified files

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://astronaut.github.io",
+  base: "my-repo",
   integrations: [tailwind()],
   env: {
     schema: {
